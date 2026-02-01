79c795fc50696633f371b13d7566718a5a82ffcf
garciasb <garciasb@rose-hulman.edu>
2026-02-01 13:23:08 -0500

    Tried to fix stuff (merge conflicts)

--- src/model/Player.java
+++ src/model/Player.java
@@ -9,17 +9,15 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 
-public class Player {
-	
-	private int x, y;
-	private int dx = 4; // direction + speed, 4 pixels per move
-	private int dy = 4; // direction + speed
-	
-	private int width = 50;  
-	private int height = 50;
+public class Player extends Entity{
 	
 	private static BufferedImage sprite = null;
 	private static boolean triedLoad = false;
+	
+	public Player(int xl, int yl, int xs, int ys) {
+		super(xl, yl, xs, ys);
+		loadSpriteOnce();
+		}
 
 	private static void loadSpriteOnce() {
 		if (triedLoad) return;
@@ -37,62 +35,43 @@ public class Player {
 
 		if (sprite != null) {
 		// sprite replaces the circle
-		g2.drawImage(sprite, x, y, width, height, null);
+		g2.drawImage(sprite, GetLocation()[0], GetLocation()[1], GetSize()[0], GetSize()[1], null);
 		} else {
 		// fallback if sprite failed to load
 		g2.setColor(Color.RED);
-		g2.fillRect(x, y, width, height);
+		g2.fillRect(GetLocation()[0], GetLocation()[1], GetSize()[0], GetSize()[1]);
 		}
-		}
-	
-	public void moveRight() {
-		  x += dx;
-		}
-	
-	public void moveLeft() {
-		  x += -dx;
-	}
-	
-	public void moveUp() {
-		  y += -dy;
-		}
-	
-	public void moveDown() {
-		  y += +dy;
 	}
 
 
 	public void update(int worldWidth, int worldHeight) {
+		System.out.println("Updating");
+		
+		// move first
+				SetLocation(GetLocation()[0]+GetSpeed()[0], 
+						GetLocation()[1]+GetSpeed()[1]);
 
-	// Left wall
-	if (x < 0) {
-	x = 0; // clamp
-	dx = 0;
-	}
-	// Right wall
-	else if (x > worldWidth) {
-	x = worldWidth;
-	dx = 0;
-	}
+				// Left wall
+				if (GetLocation()[0] < 0) {
+				SetLocation(0, GetLocation()[1]); // clamp
+				SetSpeed(0, GetSpeed()[1]);
+				}
+				// Right wall
+				else if (GetLocation()[0] + GetSize()[0] > worldWidth) {
+				SetLocation(worldWidth - GetSize()[0], GetLocation()[1]); 
+				SetSpeed(0, GetSpeed()[1]);
+				}
 
-
-	// Top wall
-	if (y < 0) {
-	y = 0;
-	dy = 0;
+				// Top wall
+				if (GetLocation()[1] < 0) {
+				SetLocation(GetLocation()[0],0); // clamp
+				SetSpeed(GetSpeed()[0],0);
+				}
+				// Bottom wall
+				else if (GetLocation()[1] + GetSize()[1] > worldHeight) {
+				SetLocation(GetLocation()[0], worldHeight - GetSize()[1]); 
+				SetSpeed(GetSpeed()[0],0);
+				}
 	}
-	// Bottom wall
-	else if (y > worldHeight) {
-	y = worldHeight;
-	dy = 0;
-	}
-	}
-	
-	
-	public Player(int x, int y) {
-		this.x = x;
-		this.y = y;
-		loadSpriteOnce();
-		}
 
 }
