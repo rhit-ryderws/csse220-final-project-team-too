29390c8dd25accb160b3ca3fbf109845ee3c0cc4
RHIT-fenga <fenga@rose-hulman.edu>
2026-02-01 13:44:32 -0500

    Made the sprite in enemy

--- src/model/Enemy.java
+++ src/model/Enemy.java
@@ -2,6 +2,10 @@ package model;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Enemy extends Entity{
 	
@@ -9,13 +13,36 @@ public class Enemy extends Entity{
 	private int[] size = GetSize();
 	private int[] position = GetLocation();
 	
+	private static BufferedImage sprite = null;
+	private static boolean triedLoad = false;
+	
 	public Enemy(int xl, int yl, int xs, int ys) {
 		super(xl, yl, xs, ys);
+		loadSpriteOnce();
+	}
+	
+	private static void loadSpriteOnce() {
+		if (triedLoad) return;
+		triedLoad = true;
+
+		try {
+
+		sprite = ImageIO.read(Square.class.getResource("ChatGPT_Zombie.png"));
+		} catch (IOException | IllegalArgumentException ex) {
+		sprite = null; 
+		}
 	}
 	
 	public void draw(Graphics2D g2) {
+		if (sprite != null) {
+		// sprite replaces the circle
+		g2.drawImage(sprite, GetLocation()[0], GetLocation()[1], GetSize()[0], GetSize()[1], null);
+		
+		} else {
+		// fallback if sprite failed to load
 		g2.setColor(Color.BLUE);
-		g2.fill(null);
+		g2.fillRect(position[0], position[1], size[0], size[1]);
+		}
 	}
 	
 	public void move() {
